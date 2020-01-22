4bb79631738dad626a4c729ccbb7749dad0893a8
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 04:48:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 require_relative './spec/fizzbuzz_spec.rb'
 
-
+=begin
 puts "Find a number that will get you to FIZZBUZZ."
 
 def fizzbuzz(int)
@@ -22,3 +22,28 @@ end
 
 
 fizzbuzz(3)
+=end
+
+
+
+
+puts('Lets play fizzbuzz')
+print('enter a number: ')
+number = gets()
+puts(number)
+if number == % 3
+  puts ('fizz')
+elsif number == % 5
+  puts ('buzz')
+elsif number == %15
+  puts ('fizzbuzz')
+end
+
+
+
+
+
+
+
+
+
