a1fa14c02769c11bcc0934c2e19d30800aa776b2
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 04:40:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,14 @@
 require_relative './spec/fizzbuzz_spec.rb'
 
 
-
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
-
 puts "Find a number that will get you to FIZZBUZZ."
 
 def fizzbuzz (int)
-  if int % 3 
+  if int / 3 
     puts "Fizz"
-  elsif int % 5
+  elsif int / 5
     puts "Buzz"
-  elsif int % 15
+  elsif int / 15
     puts "FIZZBUZZ"
   else
     puts "nil"
