19c9eb58438c6a99baf632cd8a99bba168f34971
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 04:45:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,16 +3,21 @@ require_relative './spec/fizzbuzz_spec.rb'
 
 puts "Find a number that will get you to FIZZBUZZ."
 
-def fizzbuzz (int)
+def fizzbuzz(int)
+  
   if int % 3  
     puts "Fizz"
+  
   elsif int % 5  
     puts "Buzz"
+  
   elsif int % 15 
     puts "FizzBuzz"
+  
   else
     puts "nil"
-end
+ 
+ end
 end
 
 
